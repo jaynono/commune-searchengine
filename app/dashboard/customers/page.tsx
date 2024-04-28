99e1b9f54a8039942dcb3c1d7d994699df3acb27
jaynono <senono58522@gmail.com>
2024-04-29 08:59:04 +0900

    fix_forverceldeployment

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -3,14 +3,8 @@ import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 import { Suspense } from 'react';
 import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
-import CardWrapper from '@/app/ui/dashboard/cards';
-import Image from 'next/image';
-import data from '@/app/lib/placeholder-data'
-import matchimg from '@/public/match.jpg'
-import { Card } from '@/app/ui/dashboard/cards';
+// import  {Card}  from '@/app/ui/dashboard/cards';
 import { matchlist } from '@/app/lib/placeholder-data';
-import { SignalIcon, } from '@heroicons/react/24/outline';
-import Search from '@/app/ui/search';
 
 export default function Page() {
     
@@ -25,10 +19,67 @@ export default function Page() {
                         {data.map((data) => (
                             <Card key={data.id} teamlogo1={data.teamlogo1} teamlogo2={data.teamlogo2} teamname1={data.teamname1} teamname2={data.teamname2} time={data.time} />
                         ))}
-                        {/* <Card /><Card /><Card /><Card /><Card /><Card /><Card /><Card /><Card /> */}
                     </div>
                 </Suspense>
             </div>
         </main>
     );
     }
+
+    export function Card(
+        {
+        teamlogo1,
+        teamlogo2,
+        teamname1,
+        teamname2,
+        time,
+      }: {
+        teamlogo1: string;
+        teamlogo2: string;
+        teamname1: string;
+        teamname2: string;
+        time: string;
+      }
+      ) {
+        return (
+          <>
+          <a href="#">
+            <div className="flex flex-col justify-around w-[350px] h-[200px] text-gray-300 dark:text-white rounded-xl p-2 shadow-sm backdrop-blur-lg bg-opacity-50 bg-custom-blue dark:bg-blue-900 dark:backdrop-blur-lg dark:bg-opacity-50  mb-2">
+              <div className="flex justify-center p-2 pb-0">
+                {/* {Icon ? <Icon className="h-5 w-5 text-gray-700" /> : null} */}
+                {/* <h3 className="ml-2 text-sm font-medium">werwer</h3> */}
+                <span>{time}</span>
+              </div>
+              <div className='flex flex-row justify-around items-center'>
+                <div className='flex flex-col items-center '>
+                  <div className='flex items-center justify-center w-[100px] h-[100px] mb-2'>
+                    <img
+                    src = {teamlogo1}
+                    className="inline-block hidden w-[100px] h-[100px] md:block"
+                    // width={80}
+                    // height={80}
+                    alt="Screenshots of the dashboard project showing desktop version"
+                    />
+                  </div>
+                  <div>{teamname1}</div>
+                </div>
+                <div>VS</div>
+                <div className='flex flex-col items-center'>
+                  <div className='flex items-center justify-center w-[100px] h-[100px] mb-2'>
+                    <img
+                    src= {teamlogo2}
+                    className="inline-block hidden md:block w-[100px] h-[100px]"
+                    // width={80}
+                    // height={80}
+                    alt="Screenshots of the dashboard project showing desktop version"
+                    />
+                  </div>
+                  <div>{teamname2}</div>
+                </div>
+              </div>
+            </div>
+          </a>
+          </>
+        );
+      }
+      
